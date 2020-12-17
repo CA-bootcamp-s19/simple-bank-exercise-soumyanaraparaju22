2bd355dd4d6363cbea3b9d43be080fcb1e1faa4e
soumyanaraparaju22 <42579701+soumyanaraparaju22@users.noreply.github.com>
2020-12-17 13:01:37 +0530

    Update SimpleBank.sol

--- contracts/SimpleBank.sol
+++ contracts/SimpleBank.sol
@@ -4,7 +4,8 @@
     https://solidity.readthedocs.io/en/v0.6.12/060-breaking-changes.html
 */
 
-pragma solidity ^0.6.12;
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.21 <0.7.0;
 
 contract SimpleBank {
 
@@ -13,7 +14,7 @@ contract SimpleBank {
     //
     
     /* Fill in the keyword. Hint: We want to protect our users balance from other contracts*/
-    mapping (address => uint) private balances;
+    mapping (address => uint) internal balances;
     
     /* Fill in the keyword. We want to create a getter function and allow contracts to be able to see if a user is enrolled.  */
     mapping (address => bool) public enrolled;
@@ -33,16 +34,16 @@ contract SimpleBank {
 
     /* Create an event called LogWithdrawal */
     /* Add 3 arguments for this event, an accountAddress, withdrawAmount and a newBalance */
-    event LogWithdrawal(address accountAddress,uint withdrawAmount,uint newBalance);
+    event LogWithdrawal(address accountAddress, uint withdrawAmount, uint newBalance);
 
     //
     // Functions
     //
 
     /* Use the appropriate global variable to get the sender of the transaction */
-    constructor() public {
+    constructor() payable public {
         /* Set the owner to the creator of this contract */
-        owner=msg.sender;
+        owner = msg.sender;
     }
 
     // Fallback function - Called if other functions don't match call or
@@ -58,7 +59,7 @@ contract SimpleBank {
     /// @return The balance of the user
     // A SPECIAL KEYWORD prevents function from editing state variables;
     // allows function to run locally/off blockchain
-    function getBalance() public returns (uint) {
+    function getBalance() public view returns (uint) {
         /* Get the balance of the sender of this transaction */
         return balances[msg.sender];
     }
@@ -66,11 +67,10 @@ contract SimpleBank {
     /// @notice Enroll a customer with the bank
     /// @return The users enrolled status
     // Emit the appropriate event
-    function enroll() public returns (bool){
-    require(balances[msg.sender]==0);
-    enrolled[msg.sender]=true;
-    emit LogEnrolled(msg.sender);
-    return enrolled[msg.sender];
+    function enroll() public returns (bool) {
+        enrolled[msg.sender] = true;
+        emit LogEnrolled(msg.sender);
+        return enrolled[msg.sender];
     }
 
     /// @notice Deposit ether into bank
@@ -79,13 +79,13 @@ contract SimpleBank {
     // Use the appropriate global variables to get the transaction sender and value
     // Emit the appropriate event    
     // Users should be enrolled before they can make deposits
-    function deposit() public returns (uint) {
+    function deposit() public payable returns (uint) {
         /* Add the amount to the user's balance, call the event associated with a deposit,
           then return the balance of the user */
-          require(enrolled[msg.sender]==true);
-          balances[msg.sender] +=msg.value;
-          emit LogDepositMade(msg.sender,msg.value);
-          return this.getBalance();
+        require(enrolled[msg.sender] == true);
+        balances[msg.sender] += msg.value;
+        emit LogDepositMade(msg.sender, msg.value);
+        return balances[msg.sender];
     }
 
     /// @notice Withdraw ether from bank
@@ -93,15 +93,15 @@ contract SimpleBank {
     /// @param withdrawAmount amount you want to withdraw
     /// @return The balance remaining for the user
     // Emit the appropriate event    
-    function withdraw(uint withdrawAmount) public returns (uint) {
+    function withdraw(uint withdrawAmount) public payable returns (uint) {
         /* If the sender's balance is at least the amount they want to withdraw,
            Subtract the amount from the sender's balance, and try to send that amount of ether
            to the user attempting to withdraw. 
            return the user's balance.*/
-           require(balances[msg.sender]>=withdrawAmount);
-           balances[msg.sender] -=withdrawAmount;
-           emit LogWithdrawal(msg.sender, withdrawAmount,balances[msg.sender]);
-           return this.getBalance();
+        require(balances[msg.sender] >= withdrawAmount);
+        balances[msg.sender] -= withdrawAmount;
+        emit LogWithdrawal(msg.sender, withdrawAmount, balances[msg.sender]);
+        return balances[msg.sender];
     }
 
 }
